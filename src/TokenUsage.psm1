8de375d4c615e7069405919cc0462819783dc709
duyingfang <duyingfang@reachauto.com>
2026-06-23 23:20:54 +0900

    Restore tooltip quota timing rows

--- src/TokenUsage.psm1
+++ src/TokenUsage.psm1
@@ -1925,7 +1925,9 @@ function Format-TokenUsageTooltip {
 
     $nameParts = New-Object System.Collections.Generic.List[string]
     $fiveHourPercentParts = New-Object System.Collections.Generic.List[string]
+    $fiveHourResetParts = New-Object System.Collections.Generic.List[string]
     $weeklyPercentParts = New-Object System.Collections.Generic.List[string]
+    $weeklyResetParts = New-Object System.Collections.Generic.List[string]
     foreach ($provider in @($Snapshot.Providers)) {
         if (-not $provider.Enabled) {
             continue
@@ -1933,14 +1935,27 @@ function Format-TokenUsageTooltip {
 
         $nameParts.Add((Format-TooltipProviderName -Provider $provider))
         $fiveHourPercentParts.Add((Format-TooltipPercentNumber -Value $provider.FiveHourRemainingPercent))
+
+        $fiveHourResetVal = $provider.FiveHourResetHours
+        if ($null -ne $provider.FiveHourRemainingPercent -and $provider.FiveHourRemainingPercent -ge 100) {
+            $fiveHourResetVal = 5.0
+        }
+        $fiveHourResetParts.Add((Format-TooltipTimeNumber -Value $fiveHourResetVal))
+
         $weeklyPercentParts.Add((Format-TooltipPercentNumber -Value $provider.WeeklyRemainingPercent))
+
+        $weeklyResetVal = $provider.WeeklyResetHours
+        if ($null -ne $provider.WeeklyRemainingPercent -and $provider.WeeklyRemainingPercent -ge 100) {
+            $weeklyResetVal = 168.0
+        }
+        $weeklyResetParts.Add((Format-TooltipTimeNumber -Value $weeklyResetVal -Divisor 24.0))
     }
 
     $text = 'TokenMonitor'
     if ($nameParts.Count -gt 0) {
         $text = ($nameParts -join '/')
-        $text += ("`n5h {0}" -f ($fiveHourPercentParts -join '/'))
-        $text += ("`n7d {0}" -f ($weeklyPercentParts -join '/'))
+        $text += ("`n{0} - {1}" -f ($fiveHourPercentParts -join '/'), ($fiveHourResetParts -join '/'))
+        $text += ("`n{0} - {1}" -f ($weeklyPercentParts -join '/'), ($weeklyResetParts -join '/'))
     }
 
     if ($text.Length -gt 63) {
